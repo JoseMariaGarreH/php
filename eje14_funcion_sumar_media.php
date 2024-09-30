4af8d6aaa33912a399b5fad3e44144c8411f2eee
JoseMariaGarreH <josemariagarrehermosilla5@gmail.com>
2024-09-30 17:15:10 +0200

    complete exercise

---
+++ eje14_funcion_sumar_media.php
@@ -0,0 +1,41 @@
+<?php
+// Definimos la función suma
+function suma($numeros) {
+    $resultado = 0;
+    foreach ($numeros as $numero) {
+        $resultado += $numero;
+    }
+
+    return $resultado;
+}
+// Definimos la función media
+function media($numeros) {
+    $suma = suma($numeros);
+
+    return $suma / count($numeros);
+}
+
+// Mientras haya líneas en stdin que procesar
+while($fila = fgets(STDIN)) : ?>
+<p><?php
+    $argumentos = explode(" ",trim($fila));
+    $n_arg = count($argumentos);
+    if(isset($n_arg) && $n_arg >= 3) {
+        $operacion = array_shift($argumentos);
+        switch ($operacion) {
+            case 'suma':
+                $msg = "La suma de los números es: " .suma($argumentos);
+                break;
+            case 'media':
+                $msg = "La media de los números es: " .media($argumentos);
+                break;
+            default:
+                $msg = "(Error) Operación no permitida";
+        }
+    } else {
+        $msg = "(Error) No dispongo de suficientes argumentos.";
+    }
+    echo $msg
+?>
+</p>
+<?php endwhile; ?>
