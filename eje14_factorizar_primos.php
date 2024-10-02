3d0b2a68f3eb9a0fb761eb8c16efa6002a92b93c
Alberto Sierra <alberto.sierra@murciaeduca.es>
2024-10-02 20:11:13 +0200

    complete section of functions

--- eje14_factorizar_primos.php
+++ eje14_factorizar_primos.php
@@ -1,36 +1,34 @@
 <?php
-
-    function factoresPrimos($parametro) {
-        $num_incrementado = 2;
-        $array = [];
-        $i = 0;
-
-        while($parametro <> 1){
-            if($parametro != 1){
-                if($parametro%$num_incrementado == 0){
-                    $parametro = $parametro / $num_incrementado;
-                }else{
-                    $array[$i] = $num_incrementado;
-                    $num_incrementado += $num_incrementado + 1;
-                }
+function factoresPrimos($parametro) {
+    $num_incrementado = 2;
+    $array = [];
+    
+    while ($parametro != 1) {
+        if ($parametro % $num_incrementado == 0) {
+            if (isset($array[$num_incrementado])) {
+                $array[$num_incrementado]++;
+            } else {
+                $array[$num_incrementado] = 1;
             }
-            $i++;
+            $parametro = $parametro / $num_incrementado;
+        } else {
+            $num_incrementado++;
         }
-
-        return $array;
     }
+    
+    return $array;
+}
 
-
-    function imprimeFactoresPrimos($array){
-        foreach ($array as $indice => $valor){
-            $factores_concatenados = $valor." ";
-        }
-        return $factores_concatenados;
+function imprimeFactoresPrimos($array) {
+    $factores_concatenados = "";
+    foreach ($array as $factor => $exponente) {
+        $factores_concatenados .= "$factor ^ $exponente * ";
     }
+    return rtrim($factores_concatenados, " * ");
+}
 
-    while($fila = fgets(STDIN)): ?>
-        <?php $numero = explode(" ",trim($fila));?>
-        <p><?php echo $numero ?> = <?php echo imprimeFactoresPrimos(factoresPrimos($numero)) ?></p>
-    <?php endwhile; ?>
-
+while ($fila = fgets(STDIN)): 
+    $numero = trim($fila);
+    echo "<p>$numero = " .imprimeFactoresPrimos(factoresPrimos($numero)) . "</p>\n";
+endwhile; 
 ?>
